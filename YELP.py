0088aa2d9662530d505e91e74c930c198be94395
Alex Platas <48363345+alexanderplatas@users.noreply.github.com>
2024-01-03 18:52:44 +0100

    comments

--- YELP.py
+++ YELP.py
@@ -1,8 +1,4 @@
 import pyspark
-# import pandas as pd
-# import seaborn as sns
-# import matplotlib.pyplot as plt
-
 from datetime import datetime, date
 from pyspark.sql import SparkSession, Row, Column
 from pyspark.sql.window import Window
@@ -29,83 +25,95 @@ review_path = "/data/yelp_academic_dataset_review"
 consultas_path = "/user/ec2-user/consultas/"
 
 # Verifica si las tablas existen y las crea las tablas si no existen
-print("\n ------------- Generando tabla bussiness -------------")
+print("\n Generando tabla business...", end="")
 spark.read.json(business_path_json).write.parquet(business_path, mode="overwrite")
-print("\n ------------- Se ha generado la tabla business -------------")
+print("✓")
 
-
-print("\n ------------- Generando tabla review -------------")
+print(" Generando tabla review...", end="")
 spark.read.json(review_path_json).write.parquet(review_path, mode="overwrite")
-print("\n ------------- Se ha generado la tabla review -------------")
+print("✓")
 
-# Cargar los datos de la base de datos de 
+# Cargar los datos de la base de datos de hdfs
 df_business = spark.read.parquet(business_path)
 df_review = spark.read.parquet(review_path)
 
 # Muestra los datos
-df_review.show()
-df_business.show()
+# df_review.show()
+# df_business.show()
 
-"""
-TODO Operaciones / Consultas
-"""
 
 # Consulta 1
 
-
+print("\n Realizando consulta 1...", end="")
 top_businesses = df_review.groupBy("business_id").agg(count("review_id").alias("num_reviews")) \
     .orderBy(col("num_reviews").desc()).limit(10)
-
 result1 = top_businesses.join(df_business, "business_id").select("name", "num_reviews").orderBy(col("num_reviews").desc())
-
-print("\n Realizando consulta 1:")
-print("\n \t Obtener los 10 negocios con mayor número de revisiones")
+print("✓")
+print(" Los 10 negocios con mayor número de revisiones:")
 result1.show()
 
+print(" Almacenando resultados de la consulta 1...", end="")
 result1.write.parquet(consultas_path+"consulta1", mode="overwrite")
+print("✓")
+
 
 # Consulta 2
 
+print("\n Realizando consulta 2...", end="")
 result2 = df_business.select("business_id", "stars", "categories") \
     .withColumn("category", explode(split(col("categories"), ", "))) \
     .groupBy("category").agg(avg("stars").alias("avg_stars")) \
     .orderBy(col("avg_stars").desc()).limit(10) \
     .orderBy(col("category"))
+print("✓")
 
-print("\n Realizando consulta 2:")
-print("\n \t Obtener las 10 categorías con la mayor puntuación media")
+print(" Las 10 categorías con la mayor puntuación media")
 result2.show()
+
+print(" Almacenando resultados de la consulta 2...", end="")
 result2.write.parquet(consultas_path+"consulta2", mode="overwrite")
+print("✓")
+
 
 # Consulta 3
 
+print("\n Realizando consulta 3...", end="")
 result3 = df_business.select("city", "stars") \
     .groupBy("city").agg(avg("stars").alias("avg_stars")) \
     .orderBy(col("avg_stars").desc()).limit(10) \
     .orderBy(col("city"))
+print("✓")
 
-print("\n Realizando consulta 3:")
-print("\n \t Obtener las 10 ciudades con la mayor puntuación media")
+print(" Las 10 ciudades con la mayor puntuación media:")
 result3.show()
+
+print(" Almacenando resultados de la consulta 3...", end="")
 result3.write.parquet(consultas_path+"consulta3", mode="overwrite")
+print("✓")
+
 
 # Consulta 4
 
+print("\n Realizando consulta 4...", end="")
 # Calcular la media de palabras para las reseñas de cada puntuación (1-5 estrellas)
-result4 = df_review.groupBy("stars").agg(avg(size(split(col("text"), " "))).alias("avg_words"))
+result4 = df_review.groupBy("stars").agg(avg(size(split(col("text"), " "))).alias("avg_words")).orderBy(col("stars"))
+print("✓")
 
-print("\n Realizando consulta 4:")
-print("\n \t Calcular la media de palabras para las reseñas de cada puntuación (1-5 estrellas) ")
+print(" Media de palabras para las reseñas de cada puntuación (1-5 estrellas):")
 result4.show()
+
+print(" Almacenando resultados de la consulta 4...", end="")
 result4.write.parquet(consultas_path+"consulta4", mode="overwrite")
+print("✓")
+
 
 # Consulta 5
 
 # Renombrar la columna "stars" de review_df a "review_stars"
 df_review = df_review.withColumnRenamed("stars", "review_stars")
-
 windowSpec = Window.partitionBy("review_stars").orderBy(col("count").desc())
 
+print("\n Realizando consulta 5...", end="")
 result5 = (
     df_review
     .join(df_business, "business_id")
@@ -117,25 +125,34 @@ result5 = (
     .drop("row_number")
     .orderBy("review_stars", col("count").desc())
 )
+print("✓")
 
-print("\n Realizando consulta 5:")
-print("\n \t Obtener las 10 categorías que más se repiten para cada puntuación (1-5 estrellas)")
+print(" Las 10 categorías que más se repiten para cada puntuación (1-5 estrellas):")
 result5.show(500)
+
+print(" Almacenando resultados de la consulta 5...", end="")
 result5.write.parquet(consultas_path+"consulta5", mode="overwrite")
+print("✓")
+
 
 # Consulta 6
 
-
+print("\n Realizando consulta 6...", end="")
 result6 = df_business.select("stars", "attributes.ByAppointmentOnly") \
     .groupBy("ByAppointmentOnly").agg(avg("stars").alias("avg_stars"))
+print("✓")
 
-print("\n Realizando consulta 6:")
-print("\n \t Analizar cómo un atributo determinado afecta a la puntuación del negocio")
+print(" Análisis de cómo un atributo determinado a la puntuación del negocio:")
 result6.show()
+
+print(" Almacenando resultados de la consulta 6...", end="")
 result6.write.parquet(consultas_path+"consulta6", mode="overwrite")
+print("✓")
+
 
 # Consulta 7
 
+print("\n Realizando consulta 7...", end="")
 # Obtener las 10 categorías con mayor número de reseñas
 top_categories_by_reviews = (
     df_review
@@ -171,11 +188,16 @@ result7 = (
     .drop("row_number")
     .orderBy("category", "year")
 )
-print("\n Realizando consulta 7:")
-print("\n \t Obtener la media anual de puntuación para las 10 categorías con mayor número de reseñas")
-result7.show(400)
-result7.write.parquet(consultas_path+"consulta7", mode="overwrite")
+print("✓")
 
+print(" Media anual de puntuación para las 10 categorías con mayor número de reseñas")
+result7.show(400)
+
+print(" Almacenando resultados de la consulta 7...", end="")
+result7.write.parquet(consultas_path+"consulta7", mode="overwrite")
+print("✓")
+
+print("\n Consultas realizadas, cerrando sesión de Spark...", end="")
 # Cierra la sesión de Spark
 spark.stop()
-
+print("✓\n")
